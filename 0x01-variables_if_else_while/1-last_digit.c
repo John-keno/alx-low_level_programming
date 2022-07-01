d7289a91b8be8d6ff06b959210e778364d7a228c
John <iwehjohn@gmail.com>
2022-07-01 14:05:23 -0700

    Last number in C

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Description: Random prints the last digit of number
+ *
+ * Returns: always 0 (success)
+ */
+
+int main(void)
+{
+	int n;
+	int nLast;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	nLast = n % 10;
+
+	if (nLast > 5)
+		printf("Last digit of %d is %d and is greater than 5\n", n, nLast);
+	else if (nLast == 0)
+		printf("Last digit of %d is %d and is 0\n", n, nLast);
+	else if (nLast < 6)
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, nLast);
+	return (0);
+}
