386a31f63b0d56e772b45b4fd3dccd2cec168b4a
John Keno Iweh <iwehjohn@gmail.com>
2022-07-17 05:03:38 -0700

    fix to betty

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -2,8 +2,8 @@
 
 /**
  * mul - multiplier function
- * @m: Param int number 1
- * @n: Param int number 2
+ * @a: Param int number 1
+ * @b: Param int number 2
  * Return: result (a multipied by b)
  */
 int mul(int a, int b)
