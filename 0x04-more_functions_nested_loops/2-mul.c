03603b5aced99bfff1d982a5039f458f8337ddd4
John Keno Iweh <iwehjohn@gmail.com>
2022-07-17 04:59:50 -0700

    multiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * mul - multiplier function
+ * @m: Param int number 1
+ * @n: Param int number 2
+ * Return: result (a multipied by b)
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
