a9baf81482c1de37d5272af3dde6e5d3e6659cd5
John Keno Iweh <iwehjohn@gmail.com>
2022-07-17 06:11:41 -0700

    numbers without 2 and 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints the numbers 0 to 9
+ * excluding 2 and 4
+ *
+ * Return: Always void
+ */
+void print_most_numbers(void)
+{
+	int x;
+
+	for (x = 48; x <= 57; x++)
+	{
+		if (x == 50 || x == 52)
+		{
+			x++;
+		}
+		_putchar(x);
+	}
+	_putchar('\n');
+}
